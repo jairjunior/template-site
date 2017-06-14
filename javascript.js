b8c3829b22a8fc49fa9dd61e9980a24a991d1220
Jair Junior <contato@jairjunior.eng.br>
2017-06-14 18:33:46 -0300

    Template da página principal já bem estruturada com Bootstrap.

---
+++ javascript.js
@@ -0,0 +1,11 @@
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
+
+
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  e.target // newly activated tab
+  e.relatedTarget // previous active tab
+});
