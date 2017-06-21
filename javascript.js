ec27cd23a78524c3199875921f20528d71e566ca
Jair Junior <contato@jairjunior.eng.br>
2017-06-21 17:22:47 -0300

    Adicionado efeito na barra de navegação superior. Também foi adicionada a classe my-nowrap aos textos do meu dropdown da barra de navegação.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,34 @@
 "use strict";
 
-
+var contador = 0;
 $(document).ready(function() {
 	
-	// Pega o offset da barra de navegação em relação ao topo da página (quando em "repouso")
-	var navOffsetTop = $('.my-navbar-wrapper').offset().top;	
+  // Pega o offset da barra de navegação em relação ao topo da página quando em "repouso"
+  var navbarOffsetTop = $('.my-navbar-wrapper').offset().top;	
+  // Estado da barra de navegação
+  var isDown = false;
+
 	// Função que decide quando a barra de navegação deve estar fixa ao topo ou não
 	function stickyNav(){
-		
-		var scrollOffsetTop = $(window).scrollTop();	// Posição atual em relação ao topo
 
-	    if (scrollOffsetTop > navOffsetTop)
-	        $('.my-navbar-wrapper').addClass('my-sticky-navbar');
-	    else
+		// Posição atual em relação ao topo
+		var scrollOffsetTop = $(window).scrollTop();
+
+	    if ( (scrollOffsetTop > navbarOffsetTop-10) && !isDown ) {
+	    	$('.my-navbar-wrapper')
+	        .addClass('my-sticky-navbar')
+	        .hide()
+	        .delay(200)
+	        .slideDown(500);
+	        contador ++;
+	        console.log("Número de vezes: "+contador);
+	        isDown = true;
+	    }
+
+	    else if ( (scrollOffsetTop < navbarOffsetTop) && isDown ) {
 	        $('.my-navbar-wrapper').removeClass('my-sticky-navbar'); 
+	        isDown = false;
+	    }
 	};
 
 	// Executa a função assim que o documento estiver pronto
